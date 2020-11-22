2397efff8fd98d34725c01e8263345f2bd95fcdf
y4shiro <y4shironao@gmail.com>
2020-11-22 18:25:29 +0900

    class component に書き換え

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
-function App() {
-  return <h1>Hello, World.</h1>;
+import { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return <h1>Hello, world!</h1>;
+  }
 }
 
 export default App;

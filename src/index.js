0ba9f7f771fe16fb665c846fb9449c8bec337311
y4shiro <y4shironao@gmail.com>
2020-11-24 17:57:29 +0900

    implement event submit

--- src/index.js
+++ src/index.js
@@ -14,16 +14,14 @@ import reportWebVitals from './reportWebVitals';
 const store = createStore(reducer, applyMiddleware(thunk));
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/events/new" component={EventsNew} />
-          <Route exact path="/" component={EventsIndex} />
-        </Switch>
-      </BrowserRouter>
-    </Provider>
-  </React.StrictMode>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/events/new" component={EventsNew} />
+        <Route exact path="/" component={EventsIndex} />
+      </Switch>
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 );
 
